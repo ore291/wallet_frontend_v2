ba1c063e6f637acbb9cf5975b7dab74402a6d517
ore291 <oreoluwapadonu@gmail.com>
2022-01-23 14:48:56 +0100

    .

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
     if (item) {
       let id = item.data.user.id;
       let token = item.data.tokens.access.token;
-      fetchData(id, token)
+      fetchData(id, token);
     }
   }, []);
 
