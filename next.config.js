d10c93661ed958b425a3c2f60ed2442516c5e316
ore291 <oreoluwapadonu@gmail.com>
2022-01-23 13:40:25 +0100

    .

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ module.exports = {
   publicRuntimeConfig: {
       apiUrl: process.env.NODE_ENV === 'development'
           ? 'http://localhost:3000/v1' // development api
-          : 'http://localhost:3000/v1' // production api
+          : 'https://wallet-backend-v2.herokuapp.com/v1' // production api
   }
 }
